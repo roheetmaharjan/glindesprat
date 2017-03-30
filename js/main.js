f6ee0aa1dd5aa8e7adb94942b5e052f5ea9a338a
Rohit Maharjan <swift.rohit13@gmail.com>
2017-03-30 13:58:21 +0545

    main js files updates

--- js/main.js
+++ js/main.js
@@ -27,3 +27,21 @@ $(document).ready(function(){
     prevText: '<'
   });
 });
+
+$(function(){
+    $(".dropdown").hover(            
+            function() {
+                $('.dropdown-menu', this).stop( true, true ).fadeIn("fast");
+                $(this).toggleClass('open');
+                $('b', this).toggleClass("caret caret-up");                
+            },
+            function() {
+                $('.dropdown-menu', this).stop( true, true ).fadeOut("fast");
+                $(this).toggleClass('open');
+                $('b', this).toggleClass("caret caret-up");                
+            });
+    });
+
+$("#dropdown").change(function(){
+    $("#receiver").val($(this).val());
+});
